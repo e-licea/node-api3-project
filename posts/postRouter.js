1989675b1640cdca95a7051b360aa9b2870522a6
Esteban <esteban@estebans-mbp.lan>
2021-01-20 23:41:03 -0600

    ...

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,16 @@
 const express = require('express');
+import { post } from '../server';
+import postDb from './postDb';
 
 const router = express.Router();
 
+
+
 router.get('/', (req, res) => {
   // do your magic!
+  const posts = postDb.get()
+  
+  res.status(200).json({posts: posts})
 });
 
 router.get('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
 
+const express = require('express');
 const server = express();
+
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +10,14 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`user-agent: ${req.method}, url: ${req.url}, time: ${new Date().getTime()}`)
+  next()
+}
+
+function validateUserID (req, res, next){
+    console.log(`Hello There`)
+  next();
+}
 
 module.exports = server;
